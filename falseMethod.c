78d081b7cf39a1bc970d8ce6cb6b2bf96ab16dcd
Ankan Das <113364356+ankandas-git@users.noreply.github.com>
2023-10-12 07:49:09 +0600

    false method

---
+++ falseMethod.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float func(float x){
+
+    return pow(x,3) - (2 * x) - 5;
+
+    }
+
+    float c(float a,float b,float f_a,float f_b){
+         float c = (a* f_b - b*f_a) / (f_b -f_a);
+         return c;
+    }
+
+
+    void falseMethod(float a,float b,int n){
+    //float a,b;
+
+    int i =1;
+        do{
+        printf("\n\n---------------------------- \n");
+        printf("For Iteration %d :\n\n",i);
+        printf("a = %.2f b= %.2f \n",a,b);
+
+
+        float f_a = func(a);
+        float f_b = func(b);
+        float C = c(a, b, f_a, f_b);
+        float f_c = func(C);
+
+
+        if(f_c < 0){
+            a = C;
+        }
+        else{
+            b=C;
+        }
+
+        printf("f(a)= %.5f f(b)=%.5f \n",f_a,f_b);
+        printf("C =  %.5f\n",C);
+        printf("f(c) =  %.5f\n",f_c);
+
+        i++;
+        }while(i<=n);//loop end
+    }
+
+
+
+int main()
+{
+    printf("Given that: x^3 -2x - 5 \n");
+    int n;
+    float a,b;
+    printf("input value of a and b :\n");
+    scanf("%f%f",&a,&b);
+    printf("How many iteration you want: \n");
+    scanf("%d",&n);
+
+    // f(a), f(b)
+    falseMethod(a,b,n);
+
+
+    return 0;
+}
